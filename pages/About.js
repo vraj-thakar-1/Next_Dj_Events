99008cc7b60c3a3c9d18d8df0690f20a35b51518
Vraj Thakar <vrajthakar123@gmail.com>
2023-03-24 18:47:27 +0530

    first day

---
+++ pages/About.js
@@ -0,0 +1,11 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p> This is an app to find the latest DJ and other musical event</p>
+      <p>version 1.0.0</p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ pages/events/[slug].js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Event() {
+  return (
+    <div>
+      <h1>My Event</h1>
+    </div>
+  );
+}
